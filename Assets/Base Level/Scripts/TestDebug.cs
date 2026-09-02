71388bde151267e35581c45bd939c6f59dc0be72
huntingtonrobbie <hunti007@csusm.edu>
2026-09-02 10:42:18 -0700

    HW1: import Base Runner, add burger collectable with trigger collider

---
+++ Assets/Base Level/Scripts/TestDebug.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class TestDebug : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Test test");
+    }
+
+    void Update()
+    {
+        Debug.Log("This is an update");
+    }
+}
